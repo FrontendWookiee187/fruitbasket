9e5e99c493eb35f1d931541e27bafbcbf91343ae
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-05-21 16:50:09 +0200

    added code

--- fruitbasket/src/app/app.routes.ts
+++ fruitbasket/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FruitslistComponent } from './fruitslist/fruitslist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: FruitslistComponent},
+];
